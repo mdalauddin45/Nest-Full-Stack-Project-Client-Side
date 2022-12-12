7210900ae5e2737771bac53f9e1a6ddea4690630
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-12 23:56:08 +0600

    Nav design add

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,4 +1,11 @@
 import React from "react";
+import logo from "../../assets/logo.svg";
+import {
+  ArrowPathIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   return (
@@ -6,7 +13,7 @@ const Navbar = () => {
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -18,7 +25,7 @@ const Navbar = () => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
+                  d="M4 6h16M4 12h8m-8 6h16"
                 />
               </svg>
             </label>
@@ -27,56 +34,46 @@ const Navbar = () => {
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Homepage</a>
+                <a>Home</a>
               </li>
+
               <li>
-                <a>Portfolio</a>
-              </li>
-              <li>
-                <a>About</a>
+                <a>Shop</a>
               </li>
             </ul>
           </div>
+          <div>
+            <img className="h-12" src={logo} alt="..." />
+          </div>
         </div>
-        <div className="navbar-center">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button>
+        <div className="navbar-center hidden lg:flex"></div>
+        <div className="navbar-end lg:flex">
+          <ul className="menu menu-horizontal px-1 ">
+            <li className="hidden lg:flex">
+              <h1>
+                <ArrowPathIcon className="h-6 w-6" />
+                <p className="hidden lg:flex">Compare</p>
+              </h1>
+            </li>
+            <li>
+              <h1>
+                <HeartIcon className="h-6 w-6" />
+                <p className="hidden lg:flex">Wishlist</p>
+              </h1>
+            </li>
+            <li>
+              <h1>
+                <ShoppingCartIcon className="h-6 w-6" />
+                <p className="hidden lg:flex">Cart</p>
+              </h1>
+            </li>
+            <li className="hidden lg:flex">
+              <h1>
+                <UserIcon className="h-6 w-6" />
+                <p className="hidden lg:flex">Account</p>
+              </h1>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
