2ee75e7ea2f17321878aa53c8cc32581d084b2d9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-12-15 21:09:27 +0600

    react hot toast install

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import AuthProvider from "./contexts/AuthProvider";
-import "react-datepicker/dist/react-datepicker.css";
 import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
